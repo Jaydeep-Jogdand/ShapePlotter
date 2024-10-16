e64da6679c78d48ecb561be4fb3ce4ce7be3ae44
Jaydeep-Jogdand <jaydeep.jogdand@cctech.co.in>
2024-10-16 16:57:36 +0530

    Add files via upload

---
+++ Shape-Plotter/ShapePlotter/SourceFiles/ShapePlotter.cpp
@@ -0,0 +1,21 @@
+
+#include "Circle.h"
+#include "Point.h"
+
+#include "Manager.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    Manager manager;  
+    manager.handleUserInput(); 
+   
+    return 0;
+}
+
+
+
+
+
